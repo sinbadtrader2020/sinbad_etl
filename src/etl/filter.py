0e22d84a459c1a8772d28daf730745fbfc06bcf5
Hasan Ibna Akbar <bd.pothik@gmail.com>
2020-09-09 13:42:01 +0600

    coninue previous

--- src/etl/filter.py
+++ src/etl/filter.py
@@ -1,5 +1,5 @@
 from src.remote.companies_datahub import CompanyDatahub
-from src.dbconn.query import create_or_update_record
+from src.dbconn.query import create_or_update_record, get_records_partly
 from src.dbconn.dbclass import DBClassName, Company
 from src.etl.filter_nc_lookup import FilterLookupCategory
 from src.etl.filter_nis import FilterNIS
@@ -9,32 +9,49 @@ from src.etl.filter_nir import FilterNIR
 
 
 class TradingProcessor:
-    def load_companies(self):
-        # companies = CompanyDatahub().load_companies(path='https://datahub.io/core/nyse-other-listings/datapackage.json')
-        companies = CompanyDatahub().load_companies(
-            path='https://pkgstore.datahub.io/core/nyse-other-listings/7/datapackage.json')
-        url = 'https://www.alphavantage.co/query'
-        apikey = 'AOYGBU6J7IN09PCE'  # 'JFG78N1VW11CJSOW'
-        filters = [
-            # FilterLookupCategory('/Users/pothik/Repo/miscellaneous/sinbad_finance/sinbad_finance_etl/file/lookup/'),
-            # FilterNIS(url_string=url, function='INCOME_STATEMENT', apikey=apikey),
-            FilterIATR(url_string=url, function='BALANCE_SHEET', apikey=apikey),
-            FilterDR(url_string=url, function='OVERVIEW', apikey=apikey),
-            # FilterNIR(url_string=url, function='BALANCE_SHEET', apikey=apikey),
+    def __init__(self):
+        # self.url_registerred_companies = 'https://datahub.io/core/nyse-other-listings/datapackage.json'
+        self.url_registerred_companies = 'https://pkgstore.datahub.io/core/nyse-other-listings/7/datapackage.json'
+
+        self.path_lookup_csv = '/Users/pothik/Repo/miscellaneous/sinbad_finance/sinbad_finance_etl/file/lookup/'
+
+        self.url_company_report = 'https://www.alphavantage.co/query'
+        self.apikey = 'AOYGBU6J7IN09PCE'  # 'JFG78N1VW11CJSOW'
+
+        self.filters = [
+            FilterLookupCategory(self.path_lookup_csv),
+            FilterNIS(url_string=self.url_company_report, function='INCOME_STATEMENT', apikey=self.apikey),
+            FilterIATR(url_string=self.url_company_report, function='BALANCE_SHEET', apikey=self.apikey),
+            FilterDR(url_string=self.url_company_report, function='OVERVIEW', apikey=self.apikey),
+            FilterNIR(),
         ]
 
+    def load_companies(self):
+        # Load companies from Datahub and insert in DB
+        companies = CompanyDatahub().load_companies(path=self.url_registerred_companies)
         for row in companies:
             company = Company(*row)
 
-            for filter in filters:
+            # TODO check result & success
+            result, success = create_or_update_record(table_name=DBClassName.COMPANY,
+                                                      conflict_field='sf_act_symbol',
+                                                      return_field='sf_company_id',
+                                                      record=company)
+
+        iter_row = get_records_partly(table_name=DBClassName.COMPANY)
+        for row in iter_row:
+            company = Company(**row)
+
+            for filter in self.filters:
                 compliant, nc_reason = filter(company)
                 company.sf_aaoifi_compliant = compliant
+                company.sf_nc_reason = nc_reason
 
                 if not compliant:
                     print('[INFO] ' + nc_reason)
                     break
 
-            # TODO commented for testing purpose
+            # TODO check result & success
             result, success = create_or_update_record(table_name=DBClassName.COMPANY,
                                                       conflict_field='sf_act_symbol',
                                                       return_field='sf_company_id',
