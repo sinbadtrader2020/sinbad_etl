5c9e7b4e298f489d22b34658669a5910383c8c47
Hasan Ibna Akbar <bd.pothik@gmail.com>
2020-10-12 19:07:45 +0600

    fix IATR filter algorithm

--- src/etl/filter_iatr.py
+++ src/etl/filter_iatr.py
@@ -51,11 +51,6 @@ class FilterIATR:
 
             totalAssets = common.get_string_to_float(financial_report_latest["totalAssets"])
 
-            if financial_report_latest["cash"] == None:
-                return CompliantConfig.YELLOW, \
-                       common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
-                                                   "Data not adequate to decide on this symbol ({0})".format(url))
-
             cash = common.get_string_to_float(financial_report_latest["cash"])
             longTermInvestments = common.get_string_to_float(financial_report_latest["longTermInvestments"])
             shortTermInvestments = common.get_string_to_float(financial_report_latest["shortTermInvestments"])
@@ -68,39 +63,28 @@ class FilterIATR:
             company._iatr_shortTermInvestments = shortTermInvestments  # will be used in FilterNIR
 
             if totalAssets <= 0:
-                return CompliantConfig.NONCOMPLIANT, \
+                return CompliantConfig.YELLOW, \
                        common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
                                                    "Zero or Negetive 'totalAssets' ({0})".format(url))
 
             liquid_asset = cash + longTermInvestments + shortTermInvestments + netReceivables + inventory
             illiquid_asset = totalAssets - liquid_asset
 
-            if illiquid_asset == 0:
-                return CompliantConfig.YELLOW, \
-                       common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
-                                                   "Data not adequate to decide on this symbol ({0})".format(url))
-
-            ratio = illiquid_asset / totalAssets
-
             # Business Logic: Illiquid asset to total asset ratio (IATR)
-            ratio = (netReceivables + inventory + longTermInvestments + shortTermInvestments) / totalAssets
+            ratio = illiquid_asset / totalAssets
             if ratio <= 0.3:
                 return CompliantConfig.NONCOMPLIANT, \
                        common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
                                                    "According to Business Logic ({0})".format(url))
 
         except KeyError as key_error:
-            return CompliantConfig.NONCOMPLIANT, \
+            return CompliantConfig.YELLOW, \
                    common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
                                                "Not found parameter {0} ({1})".format(key_error, url))
-        except ValueError as value_error:
-            # print(result.status_code, data)
-            print("[ERROR][ValueError]", self.__class__.__name__, company.sf_act_symbol, value_error, url)
-            # TODO handle exception
-        except ZeroDivisionError as zero_division_error:
-            # print(result.status_code, data)
-            print("[ERROR][ZeroDivisionError]", self.__class__.__name__, company.sf_act_symbol, zero_division_error,
-                  url)
-            # TODO handle exception
+        except Exception as exception:
+            print("[ERROR][Exception]", self.__class__.__name__, company.sf_act_symbol, exception, url)
+            return CompliantConfig.YELLOW, \
+                   common.get_nc_reason_string(common.NonCompliantReasonCode.IATR,
+                                               "Unknown Exception found: {0} ({1})".format(exception, url))
 
         return CompliantConfig.COMPLIANT, common.CMP_CODE
